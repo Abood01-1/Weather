6da39f4e1b83090b99b09570e574007e48eaed06
MARIO <aboodhu0@gmail.com>
2024-02-27 10:39:35 -0800

    adps

--- app/api/weather.jsx
+++ app/api/weather.jsx
@@ -4,7 +4,6 @@ export const getWeather = async (lat, lon) => {
   try {
     const res = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-      // `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ff1b9a521c79fd1e8a8f4e962c17c9b6`
     );
     const data = await res.json();
     return data;
